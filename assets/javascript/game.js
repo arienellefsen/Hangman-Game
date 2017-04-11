d1eb981708128d66ffe9a6fcd65171ccd0229bc5
Ariene Ellefsen <ariene.ellefsen@themedco.com>
2017-04-11 15:25:40 -0400

    added new js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,9 @@
 //Creating my game object
 var game = {
     score: "",
-    words: {
-        w1: ["Blue"],
-        w2: ["Orange"],
-        w3: ["Black"],
-    },
+    words: ["Blue", "arienellefsen", "Red", "Orange", "Pink"],
     isPlaying: true,
+
     // create methods
     startGame: function() {
         // listen to on keyup
@@ -17,12 +14,18 @@ var game = {
             };
         };
     },
+
+
     generateWords: function() {
-        console.log("generateWords HangMan");
-        var countWords = 'only words!!';
-        console.log("Words:" + countWords);
+        console.log("generate Words HangMan");
+        var i = 2;
+        var obj = game.words[i].length;
+        //get the numberof charater for each value inside array
+        console.log("Words: " + game.words[i] + " - Number of characters:" + obj);
+
     }
 }
+
 
 console.log(game.startGame());
 console.log(game.generateWords());
